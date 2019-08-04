9f9225790ca27f3f80519262f47776a4d3dc7f66
Mariusz Mycek <m.mycek83@gmail.com>
2019-08-04 21:46:00 +0200

    Change next.config.js for github pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withSass = require('@zeit/next-sass');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
 module.exports = withSass({
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Weather-widget' : '',
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
