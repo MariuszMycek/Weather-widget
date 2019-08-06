3d0173bd13674a0846c53e3deed245bfdfe6a272
Mariusz Mycek <m.mycek83@gmail.com>
2019-08-06 15:39:53 +0200

    Change the way of creating webpack aliases

--- next.config.js
+++ next.config.js
@@ -9,12 +9,11 @@ module.exports = withSass({
       fs: 'empty',
     };
     config.plugins.push(new MomentLocalesPlugin());
-    config.resolve = {
-      alias: {
-        utils: path.resolve(__dirname, 'utils'),
-        styles: path.resolve(__dirname, 'styles'),
-        shared: path.resolve(__dirname, 'components/shared'),
-      },
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      utils: path.resolve(__dirname, 'utils'),
+      styles: path.resolve(__dirname, 'styles'),
+      shared: path.resolve(__dirname, 'components/shared'),
     };
     return config;
   },
